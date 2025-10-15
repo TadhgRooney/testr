c4ea1f2412a6d92893e55155da3edecdc90ddf9d
TadhgRooney <tadhgrooney17@gmail.com>
2025-10-15 17:44:03 +0100

    StorageInfo DUT

---
+++ android-dut/app/src/main/java/com/testr/dut/dto/StorageInfo.java
@@ -0,0 +1,8 @@
+package com.testr.dut.dto;
+
+public class StorageInfo {
+    public long totalBytes;
+    public long availableBytes;
+    public double sequentialWriteMBps;
+    public double sequentialReadMBps;
+}
