5b87441b383e3ce7a4186b8c237565c8cc197313
TadhgRooney <tadhgrooney17@gmail.com>
2025-10-17 10:38:12 +0100

    Lower minSdk so I can view on my physical phone

--- android-dut/app/build.gradle.kts
+++ android-dut/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.testr.dut"
-        minSdk = 23
+        minSdk = 21
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -60,4 +60,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
