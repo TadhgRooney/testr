f1e807803a4e6f35ac215094d9a7d8f9b9d0862a
TadhgRooney <tadhgrooney17@gmail.com>
2025-11-24 19:58:20 +0000

    set up

---
+++ testr-backend/src/test/java/com/testr_backend/TestrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testr_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
